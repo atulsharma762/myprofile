ea50edff5bb64137ac9eb4ec80c7fcfe2511624e
yamini <atul762@gmail.com>
2022-09-14 00:19:41 +0530

    Added new files

--- src/Header.js
+++ src/Header.js
@@ -6,26 +6,48 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { Typography } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import CallIcon from '@mui/icons-material/Call';
+import TextWithIcon from './components/TextWithIcon'
 export default function Header() {
   return (
-    <div className="header">
+    <div style={{ background: "white", color: "black", width:"100%" }}>
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'row',
-          p: 1,
-          m: 1,
           borderRadius: 1,
           alignItems: "center",
+          border: "1px solid red"
 
         }}
       >
-        <Avatar>A</Avatar>
-        <Typography variant="h5" sx={{paddingLeft: "1%"}}>Atul Sharma</Typography>
-        <Typography variant="caption" sx={{paddingTop: "1%", paddingLeft: "2%", alignItems: "center"}}><AccountBoxIcon/>Software Development Engineer in Test III</Typography>
-        <Typography variant="caption" sx={{paddingTop: "1%", paddingLeft: "2%"}}><AccountBoxIcon/>11+ years experience</Typography>
-        <Typography variant="caption" sx={{paddingTop: "1%", paddingLeft: "2%"}}><EmailIcon/>atul762@gmail.com</Typography>
-        <Typography variant="caption" sx={{paddingTop: "1%", paddingLeft: "2%"}}><CallIcon/>+91-893-XXX-XXX</Typography>
+        <TextWithIcon text="Atul Sharma" icon={[<Avatar>A</Avatar>]} sx={{width:"50%"}} />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            borderRadius: 1,
+            alignItems: "center",
+            border: "2px solid green",
+            width: "33%"
+          }}
+        >
+          <TextWithIcon text="Software Developer" icon={[<AccountBoxIcon />]} />
+          <TextWithIcon text="11+ years experience" icon={[<AccountBoxIcon />]} />
+
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            borderRadius: 1,
+            border: "2px solid blue",
+            alignItems: "center",
+            width: "33%"
+          }}
+        >
+          <TextWithIcon text="atul762@gmail.com" icon={[<EmailIcon />]} />
+          <TextWithIcon text="+91-893-XXX-XXX" icon={[<CallIcon />]} />
+        </Box>
       </Box>
 
 
